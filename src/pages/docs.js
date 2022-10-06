4c51c6ea28ecaa1adf36eddbc4f6656a1ddc584b
thehocinesaad <saadhocine0@gmail.com>
2022-10-06 22:35:42 +0100

    changed index page

---
+++ src/pages/docs.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './docs-index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header id="homePageHero" className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div id="homePageHeroContent" className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            id="homePageCTA"
+            className="button button--secondary button--lg"
+            to="/intro">
+            Read The Docs 📚
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Home`}
+      description="Chargily Developers documentation">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
